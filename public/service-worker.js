c90d418c9b83405d639fd857d301f6f62d0e040f
aligneddesigns <104503737+elenitaavancy@users.noreply.github.com>
2026-09-01 06:46:50 +0800

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'fst-v1'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting())
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
